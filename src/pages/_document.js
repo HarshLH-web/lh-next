c3cf2d98d815951c7122ee7f15f2143073152b39
Harsh <harshlhagency@gmail.com>
2025-03-21 11:51:02 +0530

    added all analystics and tag manager, SC code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,73 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
       <link rel="icon" href="/favicon.png" />
+
+       {/* Google Search Console */}
+       <meta
+          name="google-site-verification"
+          content="sPEeyO__WBU1GTqCRekZi7s0oqdCAc_zua75xcF6IDM"
+        />
+
+        {/* Google Tag Manager */}
+        <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TJ2TZ9C5');
+          `,
+        }}
+      />
+
+      {/* Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-YTEVWCKCZE"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YTEVWCKCZE');
+          `,
+        }}
+      />
+
+      {/* WhatsApp Chatbot */}
+      <Script
+        src="https://d3mkw6s8thqya7.cloudfront.net/integration-plugin.js"
+        id="aisensy-wa-widget"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log("WhatsApp chatbot script loaded.");
+        }}
+      />
+
+      </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJ2TZ9C5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
