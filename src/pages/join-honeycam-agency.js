0872b7b053f946614ae39cb83102ac1dcc6e9e9b
Harsh <harshlhagency@gmail.com>
2025-07-12 10:29:56 +0530

    Added honeycam on apply now pagr

--- src/pages/join-honeycam-agency.js
+++ src/pages/join-honeycam-agency.js
@@ -31,7 +31,7 @@ function JoinHoneycamAgency() {
                     
                     <div className="flex flex-col lg:flex-row">
                     <ul className="text-left list-decimal px-6 space-y-1">
-                        <li className="text-[1rem] leading-[1.25] md:text-lg text-left">Download the HoneyCam App <a href="https://play.google.com/store/apps/details?id=com.honeycam.app" className="underline text-blue-500">For Android</a> & <a href="https://apps.apple.com/in/app/honeycam-chat-and-match-friend/id1521957817" className="underline text-blue-500">For IOS</a></li>
+                        <li className="text-[1rem] leading-[1.25] md:text-lg text-left"><a href="https://play.google.com/store/apps/details?id=com.honeycam.app" className="underline text-blue-500">Download the HoneyCam App.</a></li>
                         <li className="text-[1rem] leading-[1.25] md:text-lg text-left">Register &amp; create your account.</li>
                         <li className="text-[1rem] leading-[1.25] md:text-lg text-left"><a href="https://www.honeycammcn.com/?unionId=6052&unionName=LH+Talent+Agency#/register" className="underline text-blue-500">Click on the agency registration link.</a></li>
                         <li className="text-[1rem] leading-[1.25] md:text-lg text-left">Fill in all required information:</li>
@@ -60,14 +60,14 @@ function JoinHoneycamAgency() {
                             </span>
                             Download App
                         </a>
-                        <a href="https://apps.apple.com/in/app/honeycam-chat-and-match-friend/id1521957817" className="bg-gradient-to-l from-yellow-400 to-yellow-600 text-black text-md font-normal flex items-center justify-center gap-2 btn-gradient w-fit px-4 py-2 rounded-lg">
+                        {/* <a href="https://apps.apple.com/in/app/honeycam-chat-and-match-friend/id1521957817" className="bg-gradient-to-l from-yellow-400 to-yellow-600 text-black text-md font-normal flex items-center justify-center gap-2 btn-gradient w-fit px-4 py-2 rounded-lg">
                             <span className="w-6 h-6 inline-block">
                             <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18.71 19.5C17.88 20.74 17 21.95 15.66 21.97C14.32 22 13.89 21.18 12.37 21.18C10.84 21.18 10.37 21.95 9.09997 22C7.78997 22.05 6.79997 20.68 5.95997 19.47C4.24997 17 2.93997 12.45 4.69997 9.39C5.56997 7.87 7.12997 6.91 8.81997 6.88C10.1 6.86 11.32 7.75 12.11 7.75C12.89 7.75 14.37 6.68 15.92 6.84C16.57 6.87 18.39 7.1 19.56 8.82C19.47 8.88 17.39 10.1 17.41 12.63C17.44 15.65 20.06 16.66 20.09 16.67C20.06 16.74 19.67 18.11 18.71 19.5ZM13 3.5C13.73 2.67 14.94 2.04 15.94 2C16.07 3.17 15.6 4.35 14.9 5.19C14.21 6.04 13.07 6.7 11.95 6.61C11.8 5.46 12.36 4.26 13 3.5Z"/>
                             </svg>
                             </span>
                             For IOS 
-                        </a>
+                        </a> */}
                         <a href="https://www.honeycammcn.com/?unionId=6052&unionName=LH+Talent+Agency#/register" className="bg-yellow-500 text-black text-md font-normal flex items-center justify-center gap-2 btn-gradient w-fit px-4 py-2 rounded-lg">
                             <span className="w-5 h-5 inline-block">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="currentColor">
@@ -132,7 +132,7 @@ function JoinHoneycamAgency() {
                 <div className="lg:w-1/2 lg:pr-8">
                     <h2 className="text-3xl lg:text-5xl font-bold mb-4 text-black">Host Registration Process</h2>
                     <ul className="list-decimal px-6 space-y-2">
-                        <li className="text-[1rem] leading-[1.25] md:text-lg text-left">Download the HoneyCam App <a href="https://play.google.com/store/apps/details?id=com.honeycam.app" className="underline text-blue-500">For Android</a> & <a href="https://apps.apple.com/in/app/honeycam-chat-and-match-friend/id1521957817" className="underline text-blue-500">For IOS</a></li>
+                        <li className="text-[1rem] leading-[1.25] md:text-lg text-left"><a href="https://play.google.com/store/apps/details?id=com.honeycam.app" className="underline text-blue-500">Download the HoneyCam App.</a></li>
                         <li className="text-[1rem] leading-[1.25] md:text-lg text-left">Sign up by creating a profile.</li>
                         <li className="text-[1rem] leading-[1.25] md:text-lg text-left">Go to the <strong>profile</strong>.</li>
                         <li className="text-[1rem] leading-[1.25] md:text-lg text-left">Click on <strong>&lsquo;settings&rsquo;</strong>.</li>
