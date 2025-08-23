8a2ca28faeeeedc146a65df24b61fb6257be1a6e
Harsh <harshlhagency@gmail.com>
2025-08-23 12:41:53 +0530

    Updated images for duoo app landing page

--- src/pages/duo-agency-registration.js
+++ src/pages/duo-agency-registration.js
@@ -62,7 +62,7 @@ const DuoAgencyRegistration = () => {
 
             {/* Hero Section */}
             <section className="bg-gradient-to-b from-[#FF055F] to-[#EA7005] lg:bg-[url('/duoo/hero-bg.svg')] bg-contain bg-left-top bg-no-repeat min-h-[90vh]">
-                <div className="flex flex-col-reverse lg:flex-row w-[90%] lg:w-[80%] 2xl:w-[75%] mx-auto pt-12 pb-12 lg:py-0 gap-10 lg:gap-0">
+                <div className="flex flex-col-reverse lg:flex-row w-[90%] lg:w-[80%] 2xl:w-[75%] mx-auto pt-20 pb-12 lg:py-0 gap-6 lg:gap-0">
                     <div className="w-full lg:w-[70%] 2xl:w-[70%] lg:min-h-[98vh] 2xl:min-h-[90vh] flex items-center justify-center">
                         <div className="flex flex-col items-center justify-center max-w-4xl mx-auto text-white gap-4">
                             <Image src="/duoo/logo.png" alt="Duo Agency Registration" className="w-36 lg:w-44 2xl:w-52" width={3000} height={100} />
@@ -73,12 +73,15 @@ const DuoAgencyRegistration = () => {
                             <p className="text-base lg:text-xl 2xl:text-3xl text-center font-semibold">
                             Start your journey today with the Duoo App.
                             </p>
-                            <Link href="https://play.google.com/store/apps/details?id=com.melot.meta&referrer=inviteCode=737854066" className="duoo-btn-gradient font-semibold text-white px-4 py-2 2xl:text-lg 2xl:px-6 rounded-full border-2 border-white lg:mt-4">Download Duoo</Link>
+                            <div className="flex gap-2 lg:gap-4">
+                            <Link href="https://play.google.com/store/apps/details?id=com.melot.meta&referrer=inviteCode=737854066" className="text-sm lg:text-base 2xl:text-lg duoo-btn-gradient font-semibold text-white px-4 py-2 2xl:px-6 rounded-full border-2 border-white lg:mt-4 text-center">Download Duoo</Link>
+                            <Link href="https://agent.duoo.live/#/register?parentInviteCode=019428324" className="text-sm lg:text-base 2xl:text-lg duoo-btn-gradient font-semibold text-white px-4 py-2 2xl:px-6 rounded-full border-2 border-white lg:mt-4 text-center">Join Agency</Link>
+                            </div>
 
                         </div>
                     </div>
                     <div className="flex flex-col items-center justify-center 2xl:justify-center w-full lg:w-[30%] 2xl:w-[30%]">
-                        <Image src="/duoo/duoo-app.png" alt="Duo Agency Registration" width={3000} height={8000} className="w-52 lg:w-full max-w-96" />
+                        <Image src="/duoo/duoo-app.png" alt="Duo Agency Registration" width={3000} height={8000} className="w-36 lg:w-full max-w-[18rem]" />
                     </div>
                 </div>
             </section>
@@ -115,15 +118,15 @@ const DuoAgencyRegistration = () => {
             {/*About Duoo App */}
             <section className="bg-[#FDF4DE] lg:bg-white pt-12 lg:pb-40 lg:pt-24">
                 <div className="w-[90%] mx-auto max-w-5xl relative lg:pl-20">
-                    <div className="w-full max-w-[20rem] absolute top-[-4rem] left-[-2rem] 2xl:left-[-4rem] hidden lg:block">
-                        <Image src="/duoo/about-duoo.png" alt="Duo Agency Registration" width={10000} height={8000} className="w-full max-w-[25rem]" />
+                    <div className="w-full max-w-[18rem] absolute top-[-2rem] left-[2rem] 2xl:top-[-3rem] 2xl:left-[-2rem] hidden lg:block">
+                        <Image src="/duoo/about-duoo.png" alt="Duo Agency Registration" width={10000} height={8000} className="w-full lg:max-w-[16rem] 2xl:max-w-[18rem]" />
                     </div>
 
                     <div className="w-full max-w-5xl flex flex-col justify-center bg-[#FDF4DE] lg:py-16 lg:pl-60 lg:pr-20 2xl:pr-24 rounded-[200px_200px_200px_100px]">
                         <h2 className="text-[1.75rem] lg:text-5xl 2xl:text-[52px] font-bold text-center lg:text-left">About Duoo</h2>
                         <p className="text-base lg:text-lg 2xl:text-[19px] text-center lg:text-left font-normal mt-3 lg:mt-6">Duoo is a new live streaming app that connects people around the world. Whether you&apos;re a singer, dancer, or just want to chat, Duoo gives you the opportunity. As a duoo host, show your talent, and earn rewards while doing what you love. Manage a team of streamers by becoming a duoo agent. Start your journey today and become a part of the Duoo App.</p>
 
-                        <Image src="/duoo/about-duoo-mobile.png" alt="Duo Agency Registration" width={10000} height={8000} className="w-72 lg:hidden pr-4 mx-auto mt-6" />
+                        <Image src="/duoo/about-duoo-mobile.png" alt="Duo Agency Registration" width={10000} height={8000} className="w-60 lg:hidden pl-4 mx-auto mt-6" />
                     </div>
                 </div>
             </section>
