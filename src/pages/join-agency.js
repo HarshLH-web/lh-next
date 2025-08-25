f53581321e089832bf26c2d13f5e69db25e78701
Harsh <harshlhagency@gmail.com>
2025-08-25 14:00:20 +0530

    Updated content for join-agency page for GAds

--- src/pages/join-agency.js
+++ src/pages/join-agency.js
@@ -24,18 +24,11 @@ function JoinChamet() {
             <section id="Agency" className="bg-[#ffffff] pb-12 lg:pb-20 lg:pt-0 bg-bottom scroll-mt-16">
                 <div className="w-[90%] mx-auto max-w-6xl">
                     <div className="text-center w-full md:w-[80%] lg:w-[75%] mx-auto">
-                        <h2 className="text-4xl lg:text-6xl font-bold text-[#3B0447] leading-[1.25] lg:leading-[1.25]">
-                            Chamet Agency Registration
+                        <h2 className="text-4xl lg:text-6xl font-bold text-[#3B0447] leading-[1.25] lg:leading-[1.25] mb-3 md:mb-6">
+                         Agency Partnership
                         </h2>
-                        <p>
-                            This platform provides tools for professional networking through video-based communication. It promotes responsible usage and requires all users to follow strict content guidelines.
-                            <span className="font-semibold">
-                                Sharing nudity, sexually explicit content, or engaging in restricted activities is not permitted.
-                            </span>
-                            By registering as an Agent, you can onboard and manage broadcasters and sub-Agents under your team.
-                        </p>
-                        <p>
-                            To register as an Agent, follow these steps:
+                        <p className="text-base md:text-lg">
+                        This platform provides tools for professional networking through video-based communication. We promote responsible usage and require all users to follow strict community guidelines. When you register, you can recruit and manage creators and sub-partners to build a professional network.
                         </p>
                     </div>
                     <div className="flex flex-col md:flex-row items-center justify-center gap-4 lg:gap-12 py-4 lg:py-8">
@@ -44,10 +37,11 @@ function JoinChamet() {
                             <ImageModal imageSrc="/chamet/user-name.png" imageAlt="Chamet Agent Registration Step 1 - Enter Email/Phone" width="max-w-xl" className="w-auto lg:max-w-88 object-cover cursor-pointer" />
                         </div>
                         <div>
+                            <h3 className="text-2xl lg:text-3xl font-bold text-[#3B0447] leading-[1.25] lg:leading-[1.25] mb-2 md:mb-4">How to Register as a Partner</h3>
                             <ul className="list-decimal pl-6 space-y-2 text-lg mb-6">
-                                <li>Open the Agency <a href="https://h5.ichamet.com/webH5/inviteAgent/bind.html?companyId=3338" className="text-[#9e00c2]">Registration Link</a>.</li>
+                                <li>Open the Partner <a href="https://h5.ichamet.com/webH5/inviteAgent/bind.html?companyId=3338" className="text-[#9e00c2]">Registration Link</a>.</li>
                                 <li>Enter Your <strong>E-Mail ID or Phone Number</strong><span className="text-sm"> (Email-ID is recommended for easier recovery)</span></li>
-                                <li>Click on &ldquo;Send&rdquo; to receive verification code.</li>
+                                <li>Click &ldquo;Send&rdquo; to receive verification code.</li>
                                 <li>Enter the <strong>Verification Code</strong> received.</li>
                                 <li>Create a secure <strong>Password</strong>.</li>
                                 <li>Re-enter <strong>Password</strong> to confirm.</li>
@@ -66,10 +60,10 @@ function JoinChamet() {
                             <ImageModal imageSrc="/chamet/bind-metwallet.png" imageAlt="How to bind Metwallet as Chamet Agency for payouts" width="max-w-xl" className="w-auto lg:max-w-88 object-cover cursor-pointer" />
                         </div>
                         <div>
-                            <h3 className="text-2xl lg:text-3xl font-bold text-[#3B0447] leading-[1.25] lg:leading-[1.25] max-w-[22rem]">How to Bind Your Payout Wallet to Your Agency Account?</h3>
+                            <h3 className="text-2xl lg:text-3xl font-bold text-[#3B0447] leading-[1.25] lg:leading-[1.25] max-w-[25rem]">How to Bind Your Payout Wallet</h3>
                             <div className="h-[2px] w-20 bg-[#3B0447] my-4"></div>
                             <ul className="list-decimal pl-6 space-y-2 text-lg mt-4">
-                                <li>Access your <strong>Agency Dashboard</strong></li>
+                                <li>Access your <strong>Partner Dashboard</strong></li>
                                 <li>Navigate to <strong>My Profile</strong> section</li>
                                 <li>Scroll down the page</li>
                                 <li>Click on <strong>Bind Metwallet</strong> to link your payout account</li>
@@ -118,33 +112,33 @@ function JoinChamet() {
             <section id="Agency-commission" className="bg-[#FBD2FE] py-12 lg:pt-24 lg:pb-28 scroll-mt-16">
                 <div className="mx-auto w-[90%] max-w-5xl text-center">
                     <h2 className="text-4xl lg:text-6xl font-bold text-[#3B0447] heading-shadow leading-[1.25] lg:leading-[1.25]">
-                        Chamet Agency Commission Structure
+                    Partnership Commission Structure
                     </h2>
-                    <p className="md:text-xl pt-4">The Agency commission is determined by the total income generated by all affiliated broadcasters and sub-Agents over the last 30 days. This allows Agents to earn a percentage of the revenue generated by their network.</p>
-                    <p className="md:text-xl pt-4">The commission ratio typically ranges from 5% to 30%, designed to reward Agents for their efforts in recruiting and managing talent.</p>
-                    <p className="md:text-xl pt-4"><strong>The commission ratio is structured as follows:</strong></p>
+                    <p className="md:text-xl pt-4">Your partnership commission is based on the overall performance of all affiliated creators and sub-partners over the last 30 days. This allows partners to earn a percentage of the revenue generated by their network's collective activity on the platform.</p>
+                    <p className="md:text-xl pt-4">The commission ratio is generally between 5% to 30%, designed to reward partners for their efforts in recruiting and managing talent.</p>
+                    {/* <p className="md:text-xl pt-4"><strong>The commission ratio is structured as follows:</strong></p> */}
                     {/* ImageModal component for Commission Ratio.svg */}
                     <ImageModal imageSrc="/chamet/Commission%20Ratio.svg" imageAlt="Agent Commission Structure Details" width="max-w-4xl" className="mx-auto mt-6 mb-10 cursor-pointer w-full max-w-3xl" />
 
                     <hr className="horizontal-hr mx-auto" />
-                    <h3 className="text-2xl lg:text-3xl font-bold py-3">Understanding Commission Calculation</h3>
+                    <h3 className="text-2xl lg:text-3xl font-bold py-3">Understanding Partnership Earnings</h3>
                     <hr className="horizontal-hr mx-auto" />
                     <div className="py-6 w-full max-w-4xl flex flex-col md:flex-row justify-center items-start min-h-[10rem] mx-auto">
                         <div className="md:pt-6 w-full h-full max-w-md mx-auto">
-                            <h4 className="text-lg md:text-2xl font-bold md:mb-4">Agent Commission Details</h4>
+                            <h4 className="text-lg md:text-2xl font-bold md:mb-4">Partner Commission Details</h4>
                             <p>
-                                Commissions are based on the total income earned by your broadcasters and sub-Agents within 30 days.
+                            Commissions are based on the total income earned by your creators and sub-partners within 30 days. Partners earn between 5% and 30% depending on their team&apos;s total monthly performance
                             </p>
+                            {/* <p>
+                                Partners earn between 5% to 30% depending on the team’s total monthly performance.
+                            </p> */}
+                            <p><strong>For Creators:</strong></p>
                             <p>
-                                Agents earn between 5% to 30% depending on the team’s total monthly performance.
+                                Commission = rate × total daily cashable earnings of managed creators.
                             </p>
-                            <p><strong>For Broadcasters:</strong></p>
+                            <p><strong>For Sub-Partners:</strong></p>
                             <p>
-                                Commission = rate × total daily cashable earnings of managed broadcasters.
-                            </p>
-                            <p><strong>For Sub-Agents:</strong></p>
-                            <p>
-                                Commission = (your rate − sub-Agent&apos;s rate) × sub-Agent&apos;s broadcaster income.
+                                Commission = (your rate − sub-Partner&apos;s rate) × sub-Partner&apos;s creator income.
                             </p>
                         </div>
 
@@ -152,15 +146,15 @@ function JoinChamet() {
                         <hr className="block md:hidden horizontal-hr mx-auto my-4" />
 
                         <div className="md:pt-6 w-full h-full max-w-md mx-auto">
-                            <h4 className="text-lg md:text-2xl font-bold md:mb-4">Broadcaster Earnings:</h4>
+                            <h4 className="text-lg md:text-2xl font-bold md:mb-4">Creator Earnings:</h4>
                             <p>
-                                Broadcasters earn “beans” by participating in video calls and receiving digital gifts.
+                            Creators earn “beans” by participating in live sessions and receiving digital acknowledgments. 
                             </p>
                             <p>
-                                Beans can be converted to local currency based on real-time exchange rates.
+                            These “beans” can be converted to local currency based on real-time exchange rates.
                             </p>
                             <p className="font-semibold">
-                                Note: Earnings depend on consistent activity, user engagement, and following all platform rules.
+                            Note: Earnings depend on consistent activity, audience engagement, and following all platform rules.
                             </p>
                         </div>
                     </div>
@@ -169,13 +163,13 @@ function JoinChamet() {
 
                     <div className="border-[1px] border-black rounded-lg mb-2 w-full md:w-4/5 mx-auto bg-white">
                         <button className="w-full flex justify-between items-center p-4 text-left border-b-[1px] rounded-lg focus:outline-none">
-                            <span className="text-large md:text-xl font-bold text-[#3B0447]">Expand Your Network: Invitation for Sub-Agents</span>
+                            <span className="text-large md:text-xl font-bold text-[#3B0447]">Expand Your Network: Invitation for Sub-Partners</span>
                         </button>
                         <div className=" p-4 text-left">
                         <p>
-                            Agents can refer sub-Agents who then manage their own broadcaster groups. Agents earn a share from sub-Agent performance.
+                            Partners can refer sub-partners who then manage their own creator groups. Partners earn a share from sub-partner performance. 
                             <span className="font-semibold">
-                                This is not an MLM or pyramid model.
+                            This is not a multi-level marketing (MLM) or pyramid model.
                             </span>
                             Referral links are available on the dashboard.
                         </p>
@@ -199,13 +193,13 @@ function JoinChamet() {
                             <h2 className="text-4xl lg:text-5xl font-bold text-[#3B0447] heading-shadow"><span className="text-2xl lg:text-4xl leading-[1.25] lg:leading-[1.25]">Platform</span> <br />Guidelines
                             </h2>
                             <ul className="text-left list-disc px-6 text-lg space-y-1 pt-6">
-                            <li>Agents should not register users already linked with other agents.</li>
-                            <li>Agents must not promote competing platforms.</li>
-                            <li>Broadcasters must receive full, undisclosed payments.</li>
-                            <li>
+                                <li>Partners should not register users who are already linked with other partners.</li>
+                                <li>Partners should not promote other platforms.</li>
+                                <li>Creators must receive full, undisclosed payments.</li>
+                                <li>
                                 Users must comply with community guidelines. 
                                 <span className="font-semibold">
-                                Any form of nudity, explicit content, or policy violation is strictly prohibited.
+                                Any form of sexually suggestive or explicit content, hate speech, or policy violation is strictly prohibited.
                                 </span>
                             </li>
                             </ul>
