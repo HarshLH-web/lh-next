cb7a131619098c3541a394ba0e61dad11502c2b0
Harsh <harshlhagency@gmail.com>
2025-09-15 11:37:24 +0530

    remoed SPA routing from app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,15 +25,15 @@ export default function App({ Component, pageProps }) {
   }, [router.pathname]);
 
   // SPA route change tracking for GA/GTM
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (typeof window.gtag !== "undefined") {
-        window.gtag('config', 'G-YTEVWCKCZE', { page_path: url });
-      }
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => router.events.off('routeChangeComplete', handleRouteChange);
-  }, [router.events]);
+  // useEffect(() => {
+  //   const handleRouteChange = (url) => {
+  //     if (typeof window.gtag !== "undefined") {
+  //       window.gtag('config', 'G-YTEVWCKCZE', { page_path: url });
+  //     }
+  //   };
+  //   router.events.on('routeChangeComplete', handleRouteChange);
+  //   return () => router.events.off('routeChangeComplete', handleRouteChange);
+  // }, [router.events]);
 
   return (
     <>
@@ -44,40 +44,6 @@ export default function App({ Component, pageProps }) {
           }
         `}</style>
       </Head>
-
-      {/* Google Tag Manager */}
-      {/* <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TJ2TZ9C5');
-          `,
-        }}
-      /> */}
-
-      {/* Google Analytics */}
-      {/* <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-YTEVWCKCZE"
-        strategy="afterInteractive"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-YTEVWCKCZE');
-          `,
-        }}
-      /> */}
 
       <ScrollUp />
       <Component {...pageProps} />
